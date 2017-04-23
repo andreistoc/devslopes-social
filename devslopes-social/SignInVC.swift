43d3783005eed81f47d4b2482135e477a834d63e
Andrew Istoc <andreistoc@gmail.com>
2017-04-23 10:39:41 +0300

    Initial Commit

---
+++ devslopes-social/SignInVC.swift
@@ -0,0 +1,20 @@
+//
+//  SignInVC.swift
+//  devslopes-social
+//
+//  Created by Andrew Istoc on 22/04/2017.
+//  Copyright © 2017 Andrew Istoc. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
