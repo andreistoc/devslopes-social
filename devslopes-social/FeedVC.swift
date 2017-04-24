c61223ab31cd510a6495d3d9b687b5cb2492f515
Andrew Istoc <andreistoc@gmail.com>
2017-04-24 18:26:54 +0300

    Tweaked Feed UI

--- devslopes-social/FeedVC.swift
+++ devslopes-social/FeedVC.swift
@@ -10,12 +10,28 @@ import UIKit
 import SwiftKeychainWrapper
 import Firebase
 
-class FeedVC: UIViewController {
+class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
     }
 
     override func didReceiveMemoryWarning() {
@@ -24,15 +40,7 @@ class FeedVC: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
     @IBAction func signOutTapped(_ sender: Any) {
         
         let keychainResult = KeychainWrapper.standard.removeObject(forKey: KEY_UID)
---
+++ devslopes-social/PostCell.swift
@@ -0,0 +1,27 @@
+//
+//  PostCell.swift
+//  devslopes-social
+//
+//  Created by Andrew Istoc on 24/04/2017.
+//  Copyright © 2017 Andrew Istoc. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+    
+    @IBOutlet weak var profileImg: UIImageView!
+    @IBOutlet weak var usernameLbl: UILabel!
+    @IBOutlet weak var postImg: UIImageView!
+    @IBOutlet weak var caption: UITextView!
+    @IBOutlet weak var likesLbl: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+    
+    
+   
+
+}
